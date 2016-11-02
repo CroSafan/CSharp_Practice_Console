3e8da027cd41e703365bd6454ea67c997226a2fa
Antun Safaric <crosafan@gmail.com>
2016-11-02 17:13:43 +0100

    Added project to solution:Find pi to the power of N

---
+++ Find_Pi_To_The_Power_Of_N/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Pi_To_The_Power_Of_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
