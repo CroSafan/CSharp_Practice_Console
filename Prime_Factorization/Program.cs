012928dc220fab0fcf7bf308b0f11f846138fde3
Antun Safaric <crosafan@gmail.com>
2016-11-03 21:09:02 +0100

    Added and partialy completed(it works, but large numbers are a problem) Prime_Factorization project

---
+++ Prime_Factorization/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Factorization
+{
+    class Program
+    {
+        public static bool isPrime(int x)
+        {
+            
+            for(int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+                
+                    
+            }
+            return true;
+        }
+        //Prime Factorization - Have the user enter a number 
+        //and find all Prime Factors (if there are any) and display them.
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Eneter a nubmer!");
+            int number = Convert.ToInt16(Console.ReadLine());
+            //if (isPrime(number)) Console.WriteLine("Its a prime"); else Console.WriteLine("its not a prime");
+            //works, but it is a incomplete solution, does not work on very large numbers
+            if (!isPrime(number))
+            {
+                while (number!=0)
+                {
+                    if (number % 2 == 0)
+                    {
+                        Console.Write("2,");
+                        number = number / 2;
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        Console.Write("3,");
+                        number = number / 3;
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        Console.Write("5,");
+                        number = number / 5;
+                    }
+                    else if (number % 7 == 0)
+                    {
+                        Console.Write("7,");
+                        number = number / 7;
+                    }
+                    else if (number % 9 == 0)
+                    {
+                        Console.Write("9,");
+                        number = number / 9;
+                    }
+                    else if (number % 11 == 0)
+                    {
+                        Console.Write("11,");
+                        number = number / 11;
+                    }
+                    else
+                    {
+                    Console.Write(number);
+                        number = 0;
+                    }
+
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Can not find prime factor, your entered number is  prime!");
+            }
+            Console.Read();
+        }
+
+    }
+}
