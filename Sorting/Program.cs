73c0d5f326c5368f2d68fa13b5bf945b06b81c0c
Antun Safaric <crosafan@gmail.com>
2016-11-12 17:02:44 +0100

    Added  and finished merge sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,28 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            int[] array = {100,123,55,999, 5,3,2,234,324,4234,1,2,1 };
-            array = BubbleSort(array);
+            Stopwatch sw = new Stopwatch();
+            
+            int[] array1 = {100,123,55,999, 0,55,5454,12,55,5,3,2,234,324,4234,1,2,1,1,342,234,234,12,12312,23,4334,4534534,32,1,12,3,4,5,6,3,666,43,453,234,12,4566,456 };
+            int[] array= { 100, 123, 55, 999, 0, 55, 5454, 12, 55, 5, 3, 2, 234, 324, 4234, 1, 2, 1, 1, 342, 234, 234, 12, 12312, 23, 4334, 4534534, 32, 1, 12, 3, 4, 5, 6, 3, 666, 43, 453, 234, 12, 4566, 456 };
+            sw.Start();
+            array1 = BubbleSort(array);
+            sw.Stop();
+          
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array1[i]);
+            }
+            Console.WriteLine(sw.Elapsed);
+            int[] nekaj = new int[25];
+            sw.Start();
+            MergeSort(array, nekaj, 0, array.Length - 1);
+            sw.Stop();
             for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine(array[i]);
-
             }
+            Console.WriteLine(sw.Elapsed);
             Console.Read();
 
         }
@@ -36,6 +52,49 @@ namespace Sorting
                 }
             }
             return array;
+        }
+
+        public static void MergeSort(int[] array,int[] pomPolje,int lijevo,int desno)
+        {
+            int sredina;
+            if (lijevo < desno)
+            {
+                sredina = lijevo + (desno - lijevo) / 2;
+                MergeSort(array, pomPolje, lijevo, sredina);
+                MergeSort(array, pomPolje, sredina + 1, desno);
+                Merge(array, pomPolje, lijevo, sredina + 1, desno);
+            }
+            
+        }
+
+        private static void Merge(int[] array, int[] pomPolje, int lijevo,int sredina,int desno)
+        {
+            int[] temp = new int[45];
+            int i, eol, num, pos;
+
+            eol = (sredina - 1);
+            pos = lijevo;
+            num = (desno - lijevo + 1);
+
+            while ((lijevo <= eol) && (sredina <= desno))
+            {
+                if (array[lijevo] <= array[sredina])
+                    temp[pos++] = array[lijevo++];
+                else
+                    temp[pos++] = array[sredina++];
+            }
+
+            while (lijevo <= eol)
+                temp[pos++] = array[lijevo++];
+
+            while (sredina <= desno)
+                temp[pos++] = array[sredina++];
+
+            for (i = 0; i < num; i++)
+            {
+                array[desno] = temp[desno];
+                desno--;
+            }
         }
     }
 }
