d971c6c4a5ee9ce3398973cc1af69bb9a0fde91d
Antun Safaric <crosafan@gmail.com>
2016-11-14 21:11:18 +0100

    Added palindrome checker

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+           string stringToCheck = Console.ReadLine();
+            if (IsPalindrome(stringToCheck)) Console.WriteLine("It is a palindrome");
+            else Console.WriteLine("Is not a palindrome");
+
+            Console.Read();
+        }
+
+        public static bool IsPalindrome(string toCheck)
+        {
+            string reverse = new string(toCheck.Reverse().ToArray());
+            if (toCheck == reverse) return true;
+            else return false;
+        }
+    }
+}
