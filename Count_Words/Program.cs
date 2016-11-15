64ce241ca0d4db473205a9891ce7936e4be328f3
Antun Safaric <crosafan@gmail.com>
2016-11-15 20:40:33 +0100

    Updated word count, now reads from a file

--- Count_Words/Program.cs
+++ Count_Words/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Count_Words
@@ -16,17 +18,22 @@ namespace Count_Words
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
+
         {
-            int brojac = 0;
-            string word = Console.ReadLine();
-            string[] placeholder = word.Split(' ');
-            for(int i = 0; i < placeholder.Count(); i++)
-            {
-                Console.WriteLine(placeholder[i]);
-            }
-            brojac = placeholder.Length;
-            Console.WriteLine(brojac);
+            //Username of Windows
+            string name = Environment.UserName;
+            //absolute path
+            string path = @"C:\\Users\"+name+@"\Desktop\file.txt";
+            string readText = File.ReadAllText(path); 
+            Console.WriteLine(WordCountForOneLine(readText));
             Console.Read();
+        }
+
+
+        public static int WordCountForOneLine(string sentence)
+        {
+            //regular expression for a word, only Matches(true) if it is a word, no need to check for whitespaces
+            return Regex.Matches(sentence, @"[A-Za-z0-9]+").Count;
         }
     }
 }
