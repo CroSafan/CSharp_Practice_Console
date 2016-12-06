1f0cfc0dbd764505bea6ee0f70585a04201b5649
Antun Safaric <crosafan@gmail.com>
2016-12-06 17:45:38 +0100

    Added get ip adress from web

---
+++ Get_IP_Adress/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_IP_Adress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your IP adress is{0}", GetIPAdress());
+
+            Console.Read();
+        }
+
+
+
+        private static string GetIPAdress()
+        {
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    return client.DownloadString("http://wtfismyip.com/text");
+                }
+                catch (Exception)
+                {
+
+                    return "Couldn't rettrieve your IP adress";
+                }
+            }
+
+
+        }
+    }
+}
