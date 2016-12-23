adafa6f51aab18733022f52c5b957111da080589
Antun Safaric <crosafan@gmail.com>
2016-12-23 21:23:08 +0100

    Finished Challenge294

---
+++ Challenge_294_Rack_management_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Challenge_294_Rack_management_1
+{
+    internal class Program
+    {
+        public static bool ScrabbleSolver(string scrambledInput, string input)
+        {
+            string storage = input;
+            for (int i = 0; i < scrambledInput.Length; i++)
+            {
+                int index = storage.IndexOf(scrambledInput[i]);
+                if (index == -1) continue;
+                else storage = storage.Remove(index, 1);
+            }
+            if (storage.Length == 0) return true;
+            else return false;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(ScrabbleSolver("ladilmy", "daily"));//true
+            Console.WriteLine(ScrabbleSolver("eerriin", "eerie"));//false
+            Console.WriteLine(ScrabbleSolver("orrpgma", "program"));//true
+            Console.WriteLine(ScrabbleSolver("orpgma", "program"));//false
+            Console.Read();
+        }
+    }
+}
