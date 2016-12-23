7c23bcee7a3ee005c10e505e6b07874104c3fe6a
Antun Safaric <crosafan@gmail.com>
2016-12-23 21:47:46 +0100

    Finished bonus1 of 294

--- Challenge_294_Rack_management_1/Program.cs
+++ Challenge_294_Rack_management_1/Program.cs
@@ -6,16 +6,26 @@ namespace Challenge_294_Rack_management_1
     {
         public static bool ScrabbleSolver(string scrambledInput, string input)
         {
+            int wildCardCount = 0;
             string storage = input;
             for (int i = 0; i < scrambledInput.Length; i++)
             {
-                int index = storage.IndexOf(scrambledInput[i]);
-                if (index == -1) continue;
-                else storage = storage.Remove(index, 1);
+                if (scrambledInput[i] == '?')
+                {
+                    wildCardCount++;
+                }
+                else
+                {
+                    int index = storage.IndexOf(scrambledInput[i]);
+                    if (index == -1) continue;
+                    else storage = storage.Remove(index, 1);
+                }
+
             }
-            if (storage.Length == 0) return true;
+            if (storage.Length == 0 || storage.Length == wildCardCount) return true;
             else return false;
         }
+
 
         private static void Main(string[] args)
         {
@@ -23,6 +33,11 @@ namespace Challenge_294_Rack_management_1
             Console.WriteLine(ScrabbleSolver("eerriin", "eerie"));//false
             Console.WriteLine(ScrabbleSolver("orrpgma", "program"));//true
             Console.WriteLine(ScrabbleSolver("orpgma", "program"));//false
+            Console.WriteLine(ScrabbleSolver("pizza??", "pizzazz"));//true
+            Console.WriteLine(ScrabbleSolver("piizza?", "pizzazz"));//false            
+            Console.WriteLine(ScrabbleSolver("a??????", "program"));//true
+            Console.WriteLine(ScrabbleSolver("b??????", "program"));//false
+
             Console.Read();
         }
     }
