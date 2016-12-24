a71ae840d5ed057588587e5ea01067dd9bdb3daa
Antun Safaric <crosafan@gmail.com>
2016-12-24 14:33:43 +0100

    Challenge 293 finished

---
+++ Challenge_293_Defusing_the_bomb/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_293_Defusing_the_bomb
+{
+    class Program
+    {
+        /// <summary>
+        /// https://www.reddit.com/r/dailyprogrammer/comments/5e4mde/20161121_challenge_293_easy_defusing_the_bomb/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            bool defused = false;
+            int numberOfWiresDefused = 0;
+            var wires = File.ReadAllLines(@"C:\Users\Antun\Documents\Visual Studio 2015\Projects\CSharp_Practice_Console\Challenge_293_Defusing_the_bomb\input.txt");
+            for (int i = 0; i < wires.Length; i++)
+            {
+                if (i == wires.Length - 1)
+                {
+                    numberOfWiresDefused++;
+                }
+                else
+                {
+                    switch (wires[i])
+                    {
+                        case "white":
+                            if (wires[i + 1] != "black" || wires[i + 1] != "white") numberOfWiresDefused++;
+                            break;
+                        case "red":
+                            if (wires[i + 1] == "green") numberOfWiresDefused++;
+                            break;
+                        case "black":
+                            if (wires[i + 1] != "white" || wires[i + 1] != "green" || wires[i + 1] != "orange") numberOfWiresDefused++;
+                            break;
+                        case "orange":
+                            if (wires[i + 1] == "red" || wires[i + 1] == "black") numberOfWiresDefused++;
+                            break;
+                        case "green":
+                            if (wires[i + 1] == "orange" || wires[i + 1] == "white") numberOfWiresDefused++;
+                            break;
+                        case "purple":
+                            if (wires[i + 1] != "purple" || wires[i + 1] != "green" || wires[i + 1] != "orange" || wires[i + 1] != "white") numberOfWiresDefused++;
+                            break;
+                    }
+                }
+
+            }
+            if (numberOfWiresDefused == wires.Length) defused = true;
+
+            Console.WriteLine(defused ? "Defused" : "Boom");
+            Console.Read();
+        }
+    }
+}
--- Challenge_294_Rack_management_1/Program.cs
+++ Challenge_294_Rack_management_1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Challenge_294_Rack_management_1
 {
@@ -25,6 +28,19 @@ namespace Challenge_294_Rack_management_1
             if (storage.Length == 0 || storage.Length == wildCardCount) return true;
             else return false;
         }
+        public static string LongestScrabbleWord(string input)
+        {
+            List<string> matchinWords = new List<string>();
+
+            StreamReader sr = new StreamReader(@"C:\Users\Antun\Documents\Visual Studio 2015\Projects\CSharp_Practice_Console\Challenge_294_Rack_management_1\enable1.txt");
+            while (!sr.EndOfStream)
+            {
+                if (!ScrabbleSolver(input, sr.ReadLine())) matchinWords.Add(sr.ReadLine());
+            }
+
+            matchinWords.OrderBy(x => x.Length).ToArray();
+            return matchinWords[0];
+        }
 
 
         private static void Main(string[] args)
@@ -37,7 +53,12 @@ namespace Challenge_294_Rack_management_1
             Console.WriteLine(ScrabbleSolver("piizza?", "pizzazz"));//false            
             Console.WriteLine(ScrabbleSolver("a??????", "program"));//true
             Console.WriteLine(ScrabbleSolver("b??????", "program"));//false
-
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine(LongestScrabbleWord("dcthoyueorza"));
+            Console.WriteLine(LongestScrabbleWord("uruqrnytrois"));
+            Console.WriteLine(LongestScrabbleWord("rryqeiaegicgeo??"));
+            Console.WriteLine(LongestScrabbleWord("udosjanyuiuebr??"));
+            Console.WriteLine(LongestScrabbleWord("vaakojeaietg????????"));
             Console.Read();
         }
     }
