0d9d465c1eaebd8b42bf05114e6bff24137b7e88
Antun Safaric <crosafan@gmail.com>
2016-12-28 19:02:38 +0100

    Finished Challange 287 Bonus2

--- Challenge_287_Kaprekar_Routine/Program.cs
+++ Challenge_287_Kaprekar_Routine/Program.cs
@@ -22,19 +22,52 @@ namespace Challenge_287_Kaprekar_Routine
         public static int Desc_Digits(int input)
         {
             List<int> digits = new List<int>();
-            if (input < 999) digits.Add(0);
-            while (input !=0)
+            if (input <= 999) digits.Add(0);
+            while (input != 0)
             {
-                digits.Add( input % 10);
+                digits.Add(input % 10);
                 input /= 10;
             }
-           digits= digits.OrderByDescending(number=>number).ToList();
+            digits = digits.OrderByDescending(number => number).ToList();
             return Convert.ToInt32(String.Concat(digits.ToArray()));
+        }
+        public static int Asc_Digits(int input)
+        {
+            List<int> digits = new List<int>();
+            if (input <= 999) digits.Add(0);
+            while (input != 0)
+            {
+                digits.Add(input % 10);
+                input /= 10;
+            }
+            digits = digits.OrderBy(number => number).ToList();
+            return Convert.ToInt32(String.Concat(digits.ToArray()));
+        }
+
+        public static int Kaprekar(int input)
+        {
+            int steps = 0;
+            int result = input;
+            if (input.ToString().Distinct().Count() < 2) return 0;
+            else
+            {
+                while (result != 6174)
+                {
+                    result = Desc_Digits(result) - Asc_Digits(result);
+                    steps++;
+                }
+                return steps;
+            }
+
+
+
+
+         
         }
 
         static void Main(string[] args)
         {
-          
+
             Console.WriteLine(Largest_Digit(1234));
             Console.WriteLine(Largest_Digit(3253));
             Console.WriteLine(Largest_Digit(9800));
@@ -46,6 +79,11 @@ namespace Challenge_287_Kaprekar_Routine
             Console.WriteLine(Desc_Digits(9800));
             Console.WriteLine(Desc_Digits(3333));
             Console.WriteLine(Desc_Digits(120));
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine(Kaprekar(6589));
+            Console.WriteLine(Kaprekar(5455));
+            Console.WriteLine(Kaprekar(6174));
             Console.Read();
         }
     }
