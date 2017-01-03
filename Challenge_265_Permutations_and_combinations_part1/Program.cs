8ad6eca90205ecf3bb5174d3a26fb4bf7fbaedc1
Antun Safaric <crosafan@gmail.com>
2017-01-03 19:54:55 +0100

    Copied result

---
+++ Challenge_265_Permutations_and_combinations_part1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_265_Permutations_and_combinations_part1
+{
+    class Program
+    {
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(IEnumerable<T> items, int take = 0)
+        {
+            if (take == 0) take = items.Count();
+            if (take == 1) return items.Select(x => new T[] { x });
+            return Permutations(items, take - 1).SelectMany(x => items.Where(y => !x.Contains(y)), (x1, x2) => x1.Concat(new T[] { x2 }));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Concat(Permutations(Enumerable.Range(0, 7)).Skip(3239).Take(1).Single()));
+
+            Console.Read();
+        }
+    }
+}
